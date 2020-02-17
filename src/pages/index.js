a254db3529aa11d67acecf3f6255a2f3a438af87
inovramadani <iqbal.slf@gmail.com>
2020-02-17 22:23:11 +0700

    feat: open progress report docs when clicking on banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,12 @@ const IndexPage = () => (
       <p style={{textAlign: 'center', fontSize: '1.5rem'}}>{salam}</p>
       <p>Sehubungan dengan akan dilaksanakannya pengembangan pembangunan Masjid Qolbun Saliim Topindo Buah Batu Residence Bandung, kami dari DKM Qolbun Saliim ingin memberitahukan kepada jamaah bahwa mulai Kamis 13 Februari 2020 akan dimulai aktivitas pembangunan masjid. </p>
       <div style={{ marginBottom: `1.45rem` }}>
-        <img src={img1} width={600} />
+        <a href="https://drive.google.com/drive/folders/1tqUeZ5de4_i3o6PwElfR7qpLR5MIAGXy?usp=sharing" 
+          target="_blank" 
+          rel="noreferer noopener"
+        >
+          <img src={img1} width={600} />
+        </a>
       </div>
       <p>Untuk itu kami memohon maaf jika selama proses pembangunan akan mengganggu kenyamanan jamaah dalam beribadah. Dan kami juga memohon do’a dan support dari jamaah sekalian, semoga Allah mudahkan ikhtiar kita dalam memakmurkan masjid kita tercinta. Aamiin. </p>
       <p>Atas perhatiannya kami mengucapkan terima kasih, jazaakumullaahu khairan. </p>
