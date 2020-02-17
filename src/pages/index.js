20e70b9f879dfc6ffd5cf97d7ab649360d8350f7
inovramadani <iqbal.slf@gmail.com>
2020-02-17 20:40:56 +0700

    fix: post layout to look better in mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,18 +13,16 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1 style={{ textAlign: 'center', margin: '60px auto' }}>Pengembangan Masjid Qolbun Saliim</h1>
-    <div style={{ display: 'flex', justifyContent: 'space-between'}}>
-      <div style={{ maxWidth: '60%', paddingRight: '24px' }}>
-        <p style={{textAlign: 'center', fontSize: '1.5rem'}}>{salam}</p>
-        <p>Sehubungan dengan akan dilaksanakannya pengembangan pembangunan Masjid Qolbun Saliim Topindo Buah Batu Residence Bandung, kami dari DKM Qolbun Saliim ingin memberitahukan kepada jamaah bahwa mulai Kamis 13 Februari 2020 akan dimulai aktivitas pembangunan masjid. </p>
-        <p>Untuk itu kami memohon maaf jika selama proses pembangunan akan mengganggu kenyamanan jamaah dalam beribadah. Dan kami juga memohon do’a dan support dari jamaah sekalian, semoga Allah mudahkan ikhtiar kita dalam memakmurkan masjid kita tercinta. Aamiin. </p>
-        <p>Atas perhatiannya kami mengucapkan terima kasih, jazaakumullaahu khairan. </p>
-        <hr/>
-        <p>NB: <a href="https://drive.google.com/file/d/1LXtpKEg_5ZC205GYbINfwsWh_ExiVxvS/view?usp=sharing">Surat Keputusan</a></p> 
-      </div>
+    <div style={{ textAlign: 'center', paddingRight: '24px' }}>
+      <p style={{textAlign: 'center', fontSize: '1.5rem'}}>{salam}</p>
+      <p>Sehubungan dengan akan dilaksanakannya pengembangan pembangunan Masjid Qolbun Saliim Topindo Buah Batu Residence Bandung, kami dari DKM Qolbun Saliim ingin memberitahukan kepada jamaah bahwa mulai Kamis 13 Februari 2020 akan dimulai aktivitas pembangunan masjid. </p>
       <div style={{ marginBottom: `1.45rem` }}>
-        <img src={img1} width={600} />
+        <img src={img1} width={400} />
       </div>
+      <p>Untuk itu kami memohon maaf jika selama proses pembangunan akan mengganggu kenyamanan jamaah dalam beribadah. Dan kami juga memohon do’a dan support dari jamaah sekalian, semoga Allah mudahkan ikhtiar kita dalam memakmurkan masjid kita tercinta. Aamiin. </p>
+      <p>Atas perhatiannya kami mengucapkan terima kasih, jazaakumullaahu khairan. </p>
+      <hr/>
+      <p>NB: <a href="https://drive.google.com/file/d/1LXtpKEg_5ZC205GYbINfwsWh_ExiVxvS/view?usp=sharing">Surat Keputusan</a></p> 
     </div>
     <img src={img3} />
     <div style={{ textAlign: 'center', width: '100%'}}>
